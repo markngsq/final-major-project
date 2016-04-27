8d0919edd5d6dfa5218ae5f0bd982917b1134c6f
Calvin Ng <cng@atlassian.com>
2016-04-27 21:55:08 +1000

    removed unused import

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import 'babel-polyfill';
 import React from 'react';
 import { render } from 'react-dom';
-import { Provider } from 'react-redux';
 import { browserHistory } from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
 import makeRoutes from './routes';
