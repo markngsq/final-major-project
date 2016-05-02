eeac38f085b3ae4f109c7fca1b4bf32a615bc187
markngsq <markngshiqiang@gmail.com>
2016-05-02 22:31:39 +0100

    Fixed bug with music component

--- src/components/MediaMusic/MediaMusic.js
+++ src/components/MediaMusic/MediaMusic.js
@@ -12,18 +12,7 @@ export default class MediaMusic extends Component {
       closeComponent: this.props.closeComponent.bind(this),
     };
     return (
-<<<<<<< HEAD
-
       <ComponentWrapper {...componentProps}>
-        <div>
-          <iframe width="100%" height={250} scrolling="no" frameBorder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/260546255&color=ff006d&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false" />
-        </div>
-=======
-      <ComponentWrapper {...componentProps}>
-<<<<<<< HEAD
-        <iframe width="100%" height="100%" scrolling="no" frameBorder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/48463905&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>
->>>>>>> master
-=======
         <iframe width="100%" height="100%" scrolling="no" frameBorder="no"
         src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/220583402&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false"></iframe>
         <div className="overlay">
@@ -31,7 +20,6 @@ export default class MediaMusic extends Component {
             Using the SoundCloud API, you can build layouts that take audio on the web from Soundcloud’s database. <br />
             Soundcloud enables you to grab not just a single song, but a whole playlist as well as the comments and user information.</p>
         </div>
->>>>>>> master
       </ComponentWrapper>
     );
   }
